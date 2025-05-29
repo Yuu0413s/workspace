c56c51764f0da14f26d9fb72b1dbc44e9e3cba95
Yuu0413s <y.shibata0820@gmail.com>
2025-05-29 10:12:53 +0900

    一旦完成、また後々修正

--- My_Portfolio/src/App.js
+++ My_Portfolio/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import icon from './icon.jpg';
 
 function App() {
   return (
@@ -7,11 +8,12 @@ function App() {
         <h1>Portfolio</h1>
         <p>Learning data science and programming in university...</p>
         <nav>
-          <li><a href="#About">About Me</a></li>
-          <li><a href="#qualifications">Qualifications</a></li>
-          <li><a href="#language">Programming language</a></li>
-          <li><a href="#research">Research</a></li>
-          <li><a href="#sns">SNS</a></li>
+          <li><a href="#About">自己紹介</a></li>
+          <li><a href="#career">経歴</a></li>
+          <li><a href="#skills">スキル</a></li>
+          <li><a href="#hack">ハッカソン・コンペティション</a></li>
+          <li><a href="#interns">インターンシップ</a></li>
+          <li><a href="#links">リンク</a></li>
         </nav>
       </top>
 
@@ -19,6 +21,7 @@ function App() {
         <section id="About">
           <h2>自己紹介</h2>
           <ul>
+            <img src={icon} alt="Profile Icon" className="profile-icon" />
             <blockquote>
               <strong>大学にてデータサイエンスの勉強をしながら、時系列データを用いたシステムの作成や研究を行っています。
               <p>ユーザーのQOLを向上させることのできるシステムを開発することが目標です。</p></strong>
@@ -29,46 +32,55 @@ function App() {
           </ul>
         </section>
 
-        <section id="qualifications">
-          <h2>所持資格</h2>
+        <section id="career">
+          <h2>経歴</h2>
           <ul>
-            <li>実用英語技能検定 2級(最高スコア2158)</li>
-            <li>珠算能力検定 準2級</li>
-            <li>暗算検定 3級</li>
+            <p>2021/04~2024/03 都立翔陽高校</p>
+            <p>2024/04~ 武蔵野大学データサイエンス学部データサイエンス学科</p>
           </ul>
         </section>
 
-        <section id="language">
-          <h2>学習中のプログラミング言語</h2>
+        <section id="skills">
+          <h2>スキル</h2>
           <ul>
             <li>Python</li>
-            <li>HTML</li>
-            <li>CSS</li>
+            <p>基本的なコーディングができます。Streamlitを用いたwebアプリケーションの開発も可能。</p>
+            <li>HTML/CSS</li>
+            <p>簡単なwebページをHTMLにて作成可能、CSSは使用可能だがまだ勉強中。</p>
+            <li>JavaScript</li>
+            <p>HTML/CSSに付随して学習。ﾏﾀﾞﾂｶｴﾅｲ</p>
+            <li>React</li>
+            <p>HTML/CSSが書けるので最低限使用可能。しかしまだまだ勉強不足</p>
+            <li>SQL</li>
+            <p>基本的なデータベースの操作が可能。基本SQliteを使用。</p>
           </ul>
         </section>
 
-        <section id="research">
-          <h2>現在の研究</h2>
+        <section id="hack">
+          <h2>ハッカソン・コンペティション参加経歴</h2>
           <ul>
-            <li>研究タイトル:時系列データと仕事率を用いたパーソナル管理システムの実現</li>
-            <li>研究の概要:<a href="https://docs.google.com/presentation/d/1yz86yZaYNgM084oPdGhHmK8QBR4xkimi5nGWIHKdaDI/edit?usp=sharing" target="_blank" rel="noopener noreferrer">Googleスライド</a></li>
-            <li>研究のゴール:時空間データと仕事率を用いた仕事、健康管理を行い個々の作業パフォーマンス向上と健康維持の両立を図ること</li>
-            <li>1年次の研究成果(PDFファイル):<a href="pdf.html" target="_blank" rel="noopener noreferrer">1年次成果発表会ポスター</a></li> {/* pdf.htmlはpublicフォルダに配置想定 */}
           </ul>
         </section>
 
-        <section id="sns">
-          <h2>各種SNS</h2>
+        <section id="interns">
+          <h2>インターンシップ経歴</h2>
+          <ul>
+          </ul>
+        </section>
+
+        <section id="links">
+          <h2>リンク</h2>
           <ul>
             <li>GMail : y.shibata0820@gmail.com</li>
             <li><a href="https://github.com/Yuu0413" target="_blank" rel="noopener noreferrer">Github</a></li>
             <li><a href="https://www.wantedly.com/id/yuu_0413" target="_blank" rel="noopener noreferrer">Wantedly</a></li>
           </ul>
         </section>
+
       </main>
 
       <footer>
-        <p>&copy; 2024 Yuta Shibata</p>
+        <p>&copy; 2025 Yuta Shibata</p>
       </footer>
     </div>
   );
