30fd491b42be285df16698a27d41e3cf1fe41589
Yuu0413s <y.shibata0820@gmail.com>
2025-06-27 10:57:09 +0900

    💄 基礎ファイル作成

--- sumire_endless/src/App.jsx
+++ sumire_endless/src/App.jsx
@@ -2,8 +2,22 @@ import './App.css';
 
 function App() {
     return (
-        <div>
-            <h1></h1>
+        <div ClassName='Top'>
+          <top>
+            <img src="" alt="風紀委員長?!" />
+          </top>
+
+          <main>
+            <p>まえの話</p>
+            <p>つぎの話</p>
+
+          <p><strong>このサイトについて</strong></p>
+          <li>ぶいすぽっ！の風紀委員長こと花芽すみれさんの雑談枠におけるセンシティブ発言をエンドレスにランダム再生する非公式サイトです</li>
+          <li>このサイトでは、YouTubeの公式埋め込みプレイヤーを使用しています。再生はYouTube上での視聴回数に反映され、広告が表示される場合、その収益はチャンネルに還元されます。</li>
+          <p><strong>参考にさせていただいたサイト</strong></p>
+          <li><a href="https://endless-7.netlify.app/">七瀬さんの話エンドレス</a></li>
+
+          </main>
         </div>
     );
 }
